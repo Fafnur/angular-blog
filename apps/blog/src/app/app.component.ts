38f5320313c677ed35887d741ce344492498dac9
Aleksandr Serenko <fafnur@yandex.ru>
2023-08-02 12:33:59 +0700

    [config] Adding config for workspace

--- apps/blog/src/app/app.component.ts
+++ apps/blog/src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
-  standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
   selector: 'angular-blog-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [ RouterModule],
 })
 export class AppComponent {
-  title = 'blog';
 }
--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,8 @@
 const nxPreset = require('@nx/jest/preset').default;
 
-module.exports = { ...nxPreset };
+module.exports = {
+  ...nxPreset,
+  testEnvironment: `${__dirname}/jest.jsdom.js`,
+  collectCoverage: true,
+  coverageDirectory: `${process.env.NX_WORKSPACE_ROOT}/coverage/${process.env['NX_TASK_TARGET_PROJECT']}`,
+};
