a34da901330a9e087aa800f2b9149e4a69108dc1
Aleksandr Serenko <fafnur@yandex.ru>
2023-08-03 16:47:42 +0700

    [structure] Integrating layout

--- apps/blog/src/app/app.routes.ts
+++ apps/blog/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+import { LayoutComponent } from '@angular-blog/ui/layout';
+
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [],
+  },
+];
