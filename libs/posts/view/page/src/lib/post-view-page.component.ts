05e609f8c1db796ac3975dd34d4167fb8192ccee
Aleksandr Serenko <fafnur@yandex.ru>
2023-08-08 14:44:39 +0700

    [posts] Fixing logic for post view

--- libs/posts/view/page/src/lib/post-view-page.component.ts
+++ libs/posts/view/page/src/lib/post-view-page.component.ts
@@ -1,8 +1,10 @@
 import { DatePipe, NgIf } from '@angular/common';
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, OnDestroy, OnInit, Renderer2 } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { Post } from '@angular-blog/posts/common';
+
+import { SafeHtmlPipe } from './safe-html.pipe';
 
 @Component({
   selector: 'angular-blog-post-view-page',
@@ -10,12 +12,19 @@ import { Post } from '@angular-blog/posts/common';
   styleUrls: ['./post-view-page.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [NgIf, DatePipe],
+  imports: [NgIf, DatePipe, SafeHtmlPipe],
 })
-export class PostViewPageComponent implements OnInit {
+export class PostViewPageComponent implements OnInit, OnDestroy, AfterViewInit {
   post!: Post;
 
-  constructor(private readonly activatedRoute: ActivatedRoute) {}
+  listenClickFunc!: () => void;
+
+  constructor(
+    private readonly activatedRoute: ActivatedRoute,
+    private readonly router: Router,
+    private readonly elementRef: ElementRef,
+    private readonly renderer: Renderer2
+  ) {}
 
   ngOnInit(): void {
     let route = this.activatedRoute.snapshot;
@@ -27,5 +36,22 @@ export class PostViewPageComponent implements OnInit {
     if (route.data['post']) {
       this.post = route.data['post'];
     }
+  }
+
+  ngOnDestroy() {
+    if (this.listenClickFunc) {
+      this.listenClickFunc();
+    }
+  }
+
+  ngAfterViewInit() {
+    const navigationElements = Array.prototype.slice.call(this.elementRef.nativeElement.querySelectorAll('a[routerLink]'));
+
+    navigationElements.forEach((elem) => {
+      this.listenClickFunc = this.renderer.listen(elem, 'click', (event) => {
+        event.preventDefault();
+        void this.router.navigate([elem.getAttribute('routerLink')]);
+      });
+    });
   }
 }
