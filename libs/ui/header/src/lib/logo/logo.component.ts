b3bdff281fcbc734195a1cec74942a413b760f4d
Aleksandr Serenko <fafnur@yandex.ru>
2023-08-08 18:37:46 +0700

    [i18n] Adding second part localization

--- libs/ui/header/src/lib/logo/logo.component.ts
+++ libs/ui/header/src/lib/logo/logo.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'angular-blog-logo',
@@ -7,6 +8,6 @@ import { RouterLink } from '@angular/router';
   styleUrls: ['./logo.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, MatButtonModule],
 })
 export class LogoComponent {}
